c4558e5f4edc2e4927df50b37f15c89390963998
YeiDiaz <yei.diaz93@gmail.com>
2021-03-04 01:19:00 +0000

    contador

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,28 @@
 import React from "react";
-
+import PropTypes from "prop-types";
+//import { CounterMaker } from "./CounterMaker";
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-export function Home() {
+export function Home(props) {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<i className="far fa-clock fa-4x  p-2" />
+			<div className="card1 p-2">{props.prop1}</div>
+			<div className="card2 p-2">{props.prop2}</div>
+			<div className="card3 p-2">{props.prop3}</div>
+			<div className="card4 p-2">{props.prop4}</div>
+			<div className="card5 p-2">{props.prop5}</div>
+			<div className="card6 p-2">{props.prop6}</div>
 		</div>
 	);
 }
+Home.propTypes = {
+	prop0: PropTypes.string,
+	prop1: PropTypes.string,
+	prop2: PropTypes.string,
+	prop3: PropTypes.string,
+	prop4: PropTypes.string,
+	prop5: PropTypes.string,
+	prop6: PropTypes.string
+};
